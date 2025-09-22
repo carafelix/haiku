c73b0f6cf733224d03168b470c706af6a644cebd
Hero Protagonist <heroprotagonist32@gmail.com>
2025-09-22 15:14:10 -0300

    feat: add tg post functionality

--- src/utils/tg-post.ts
+++ src/utils/tg-post.ts
@@ -1,1 +1,90 @@
 // scrip to push a channel post
+import {
+    Api,
+    InputFile,
+} from 'https://deno.land/x/grammy/mod.ts'
+import { autoRetry } from 'https://deno.land/x/grammy_auto_retry@v2.0.2/mod.ts'
+
+const SECRETS = [
+    Deno.env.get('BOT_TOKEN'),
+    Deno.env.get('CHAT_ID'),
+    Deno.env.get('POST_COUNTER'),
+]
+const [BOT_TOKEN, CHAT_ID, POST_COUNTER] = SECRETS
+
+if (!BOT_TOKEN || !CHAT_ID || !POST_COUNTER) {
+    const missing = Object.entries({
+        BOT_TOKEN,
+        CHAT_ID,
+        POST_COUNTER,
+    })
+        .filter(([key, value]) => !value)
+        .map(([key, value]) => key)
+        .join(', ')
+    throw `❌ Missing the following env variables: ${missing}. Please provide a working .env file`
+}
+
+const api = new Api(BOT_TOKEN)
+api.config.use(autoRetry())
+
+const SPECIAL_CHARS = [
+    '\\',
+    '_',
+    '*',
+    '~',
+    '`',
+    '>',
+    '<',
+    '&',
+    '#',
+    '+',
+    '-',
+    '=',
+    '{',
+    '}',
+    '.',
+    '(',
+    ')',
+    '!',
+]
+
+function escapeMarkdown(text: string) {
+    SPECIAL_CHARS.forEach(
+        (char) =>
+            (text = text.replaceAll(char, `\\${char}`))
+    )
+    return text
+}
+
+const mds = Deno.readDirSync('src/content')
+    .filter((file) => file.name.endsWith('.md'))
+    .toArray()
+
+if (+POST_COUNTER + 1 === mds.length)
+    throw 'Reached the last post of the list'
+
+const filename = mds[+POST_COUNTER].name
+const content = Deno.readTextFileSync(
+    `src/content/${filename}`
+)
+
+await api.sendPhoto(
+    CHAT_ID,
+    new InputFile(
+        `src/content/imgs/${filename.replace(
+            '.md',
+            ''
+        )}.jpg`
+    ),
+    {
+        parse_mode: 'MarkdownV2',
+        caption: escapeMarkdown(content),
+    }
+)
+
+Deno.writeTextFileSync(
+    '.env',
+    `BOT_TOKEN=${BOT_TOKEN}\nCHAT_ID=${CHAT_ID}\nPOST_COUNTER=${
+        +POST_COUNTER + 1
+    }`
+)
